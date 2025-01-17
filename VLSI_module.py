5b7362eec8046cb61fb108e940485f4a2478fa54
KIM MINHYUN <minh3789@hanyang.ac.kr>
2025-01-18 00:35:15 +0900

    Update VLSI_module.py
    modify variable type

--- VLSI_module.py
+++ VLSI_module.py
@@ -1,6 +1,6 @@
 class Module:
 
-    def __init__(self, name: int, width: float, height: float, module_type: bool, net=None):
+    def __init__(self, name: str, width: float, height: float, module_type: str, net=None):
 
         self.name = name
         self.width = width
@@ -8,7 +8,7 @@ class Module:
         self.area = width * height
         self.x = 0
         self.y = 0
-        self.type = module_type
+        self.type = module_type  # 문자열로 저장
         self.net = net if net is not None else []
 
     def set_position(self, x: float, y: float):
@@ -17,8 +17,9 @@ class Module:
         self.y = y
 
     def __str__(self):
+        # Net 데이터를 깔끔하게 정리해서 출력
+        formatted_net = "\n    ".join(self.net)
 
         return (f"Module(Name={self.name}, Width={self.width:.2f}, Height={self.height:.2f}, "
                 f"Area={self.area:.2f}, Position=({self.x:.2f}, {self.y:.2f}), "
-                f"Type={'Hard' if self.type else 'Soft'}, Net={self.net})")
-
+                f"Type={self.type}, Net=\n    {formatted_net})")
