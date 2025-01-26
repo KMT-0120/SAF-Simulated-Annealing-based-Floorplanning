87b2612a6e6471ad54a2790dbf9d032302694884
KIM MINHYUN <minh3789@hanyang.ac.kr>
2025-01-26 20:06:57 +0900

    Update class_module.py
    module 구조 수정
    배치순서 추가 등

--- class_module.py
+++ class_module.py
@@ -1,25 +1,25 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
 class Module:
-
     def __init__(self, name: str, width: float, height: float, module_type: str, net=None):
-
         self.name = name
         self.width = width
         self.height = height
         self.area = width * height
         self.x = 0
         self.y = 0
-        self.type = module_type  # 문자열로 저장
+        self.type = module_type
         self.net = net if net is not None else []
+        self.order = None  # 순서 번호 필드 추가
 
-    def set_position(self, x: float, y: float):
-
+    def set_position(self, x: float, y: float, order: int):  # 위치 설정 시 순서 번호도 설정
         self.x = x
         self.y = y
+        self.order = order
 
     def __str__(self):
-        # Net 데이터를 깔끔하게 정리해서 출력
         formatted_net = "\n    ".join(self.net)
-
         return (f"Module(Name={self.name}, Width={self.width:.2f}, Height={self.height:.2f}, "
                 f"Area={self.area:.2f}, Position=({self.x:.2f}, {self.y:.2f}), "
-                f"Type={self.type}, Net=\n    {formatted_net})")
+                f"Order={self.order}, Type={self.type}, Net=\n    {formatted_net})")
